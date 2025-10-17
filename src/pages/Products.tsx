da1c465ada93d31abd87b5ae2938d4430eb99e09
Harish C <120249766+Harishgowdaa@users.noreply.github.com>
2025-10-17 19:33:52 +0530

    Import img

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { Filter, Search } from 'lucide-react';
 import Product1 from '../assets/images/products/product-1.png';
 import Product2 from '../assets/images/products/product-2.webp';
-import Product3 from '../assets/images/products/Products-3.jpg';
+import Product3 from '../assets/images/products/products-3.jpg';
 
 const categories = ['All', 'HSS Tools', 'Carbide Tools', 'Milling Cutters', 'Drill Bits', 'Reamers', 'Custom'];
 
